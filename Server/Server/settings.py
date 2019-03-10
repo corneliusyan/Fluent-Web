248d2f1ffc65b076b2373d795a71bae930fc0c69
corneliusyan <onel.raja@gmail.com>
2019-03-11 00:20:07 +0700

    Create lecture and lesson model

--- Server/Server/settings.py
+++ Server/Server/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 
 import django_heroku
--- Server/lip/models.py
+++ Server/lip/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Lesson(models.Model):
+    lesson_number = models.IntegerField(default=0)
+    title = models.CharField(max_length=100, blank=False)
+    rating = models.FloatField(default=5)
+    rating_count = models.IntegerField(default=0)
+    students_count = models.IntegerField(default=0)
+    lectures_count = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ('number',)
+
+class Lecture(models.Model):
+    lesson_number = models.IntegerField(blank = False)
+    lecture_number = models.IntegerField(blank = False)
+    text = models.TextField()
+
+    class Meta:
+        ordering = ('lesson_number','lecture_number',)
