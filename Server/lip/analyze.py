488c51ac85cec7a0317896a7711f73c5babfea2b
corneliusyan <onel.raja@gmail.com>
2019-03-13 08:46:11 +0700

    Improve analyze algorithm

--- Server/lip/analyze.py
+++ Server/lip/analyze.py
@@ -1,6 +1,8 @@
 import json
 import string
+import re
 
+second_per_word = 0.6
 
 def analyze(input, source, time, expected_time=0):
     old_src = source
@@ -13,22 +15,29 @@ def analyze(input, source, time, expected_time=0):
     # result calculation
     # wrong words will be marked by *...*
     differences = set(source.split()) - set(input.split())
-    result = old_src
-    for dif in differences:
-        result = result.replace(dif, '*'+dif+'*')
-    response['result'] = result
+    differences = sorted(differences,key=len,reverse=True)
+    words = old_src.split()
+    result = []
+    for word in words:
+        if word.lower() in differences:
+            result.append('*' + word + '*')
+        elif word[:-1].lower() in differences:
+            result.append('*' + word[:-1] + '*' + word[-1] )
+        else:
+            result.append(word)
+    response['result'] = ' '.join(result)
     response['wrong_words'] = list(differences)
 
     # clarity calculation
     count_src = len(set(source.split()))
     count_differences = len(differences)
     clarity = (count_src-count_differences)/count_src
-    response['clarity'] = clarity
+    response['clarity'] = round(clarity,2)
 
     # pace calculation
-    # if expected_time wasn't set, replace it with <total words in source>*1 (expected_time = 1 word/second)
+    # if expected_time wasn't set, replace it with <total words in source>*second_per_word
     if expected_time == 0:
-        expected_time = len(source.split())*1
+        expected_time = len(source.split())*second_per_word
     if time-expected_time <= 0:
         pace = 1
     elif time-2*expected_time <= 0:
