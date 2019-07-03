beae5475ba8c17070326a35d8d1bff416abe9497
Rikadewi <rika_4444@yahoo.com>
2019-07-03 15:47:14 +0700

    Add dashboard home section

--- Frontend/src/Dashboard.js
+++ Frontend/src/Dashboard.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
-
+import logo from './assets/img/fluent-logo.png';
+import homeImg from './assets/img/home.png';
 class Dashboard extends Component {  
-    render() {
-      var sectionStyle = {
-        backgroundSize: "cover",
-        margin: 0,
-        height: '100vh',
-        width: '100vw',
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column',
-        textAlign: 'center',
-      };
-  
-      return (
-        <section>
-          Dashboard
-        </section>
-      );
-    }
+  render() {
+    return (
+      <section className="sectionStyle home center">
+        <div className="menu sectionStyle">
+          <div>
+            <img className="logoStyle" src={logo} />
+          </div>
+          <div className="signin-button">
+            <p>Sign In</p>
+          </div>
+        </div>
+        <div className="center content">
+          <div className="center">
+            <div className="title">
+              <div className="stripe">
+              </div>
+              <div className="content-stripe">
+                <h1>Inspiring English Future Speaker</h1>
+                <div className="join-button">
+                  <p>Join Us</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="image-container">
+            <img src={homeImg} />
+          </div>
+        </div>
+      </section>
+    );
   }
-  
-  export default Dashboard;
+}
+
+export default Dashboard;
